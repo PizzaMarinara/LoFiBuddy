41868cb5c4995b568b1b0e6f3e0e8bd258cbbf38
PizzaMarinara <pizzamarinara@protonmail.com>
2022-05-28 16:53:28 +0200

    Fading controls

--- app/src/main/java/dev/efantini/lofibuddy/presentation/viewerscreen/ViewerScreenContent.kt
+++ app/src/main/java/dev/efantini/lofibuddy/presentation/viewerscreen/ViewerScreenContent.kt
@@ -2,7 +2,13 @@ package dev.efantini.lofibuddy.presentation.viewerscreen
 
 import android.content.pm.ActivityInfo
 import android.os.Build.VERSION.SDK_INT
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -10,15 +16,18 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -41,17 +50,47 @@ import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTube
 import dev.efantini.lofibuddy.R
 import dev.efantini.lofibuddy.presentation.shared.elements.LockScreenOrientation
 import dev.efantini.lofibuddy.presentation.shared.elements.findActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @Composable
 fun ViewerScreenContent() {
     val systemUiController: SystemUiController = rememberSystemUiController()
 
+    val videoId by remember { mutableStateOf("-9gEgshJUuY") }
+    // var backgroundImageUrl by remember { mutableStateOf("-9gEgshJUuY") }
+
     var playerState by remember { mutableStateOf("") }
     var myPlayer by remember { mutableStateOf<YouTubePlayer?>(null) }
+
+    val controlsVisibility = remember { mutableStateOf(true) }
+    val coroutineScope = rememberCoroutineScope()
+
+    var fadingJob: Job? = null
+
+    val showButtonThenFade: () -> Unit = {
+        controlsVisibility.value = true
+        if (fadingJob?.isActive == true) fadingJob?.cancel()
+        fadingJob = coroutineScope.launch(Dispatchers.IO) {
+            delay(5000)
+            withContext(Dispatchers.Main) {
+                controlsVisibility.value = false
+            }
+        }
+    }
+
+    Log.d("DEBUGO", "visible? ${controlsVisibility.value}")
 
     systemUiController.isStatusBarVisible = false
     systemUiController.isNavigationBarVisible = false
     systemUiController.isSystemBarsVisible = false
+
+    LaunchedEffect("fadeControls") {
+        controlsVisibility.value = false
+    }
 
     SideEffect {
         systemUiController.setSystemBarsColor(
@@ -64,8 +103,6 @@ fun ViewerScreenContent() {
     val listener = object : AbstractYouTubePlayerListener() {
         override fun onReady(youTubePlayer: YouTubePlayer) {
             super.onReady(youTubePlayer)
-
-            val videoId = "-9gEgshJUuY"
             youTubePlayer.loadVideo(videoId, 0F)
         }
 
@@ -90,7 +127,9 @@ fun ViewerScreenContent() {
         }
         .build()
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .clickable { showButtonThenFade() },
         contentAlignment = Alignment.Center
     ) {
         AsyncImage(
@@ -103,23 +142,39 @@ fun ViewerScreenContent() {
             contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxSize()
         )
-        Image(
-            modifier = Modifier
-                .fillMaxHeight(0.2F)
-                .fillMaxHeight(0.5F)
-                .clickable {
-                    if (playerState == PlayerState.PLAYING.name)
-                        myPlayer?.pause()
-                    else
-                        myPlayer?.play()
-                },
-            painter = if (playerState == PlayerState.PLAYING.name) {
-                painterResource(id = R.drawable.pause_button)
-            } else {
-                painterResource(id = R.drawable.play_button)
-            },
-            contentDescription = ""
-        )
+        AnimatedVisibility(
+            visible = controlsVisibility.value,
+            enter = fadeIn(),
+            exit = fadeOut(
+                animationSpec = tween(
+                    durationMillis = 5000,
+                    delayMillis = 0,
+                    easing = LinearOutSlowInEasing
+                )
+            )
+        ) {
+            Box {
+                Image(
+                    modifier = Modifier
+                        .fillMaxHeight(0.2F)
+                        .fillMaxHeight(0.5F)
+                        .clickable {
+                            showButtonThenFade()
+                            if (playerState == PlayerState.PLAYING.name)
+                                myPlayer?.pause()
+                            else
+                                myPlayer?.play()
+                        },
+                    painter = if (playerState == PlayerState.PLAYING.name) {
+                        painterResource(id = R.drawable.pause_button)
+                    } else {
+                        painterResource(id = R.drawable.play_button)
+                    },
+                    contentDescription = "",
+                    colorFilter = ColorFilter.tint(Color.White)
+                )
+            }
+        }
     }
 }
 
